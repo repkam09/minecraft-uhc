3e3dcb003d5eb08fe3ae352b70ed2ff8d3dbf089
Mark Repka <mark@repkam09.com>
2016-02-24 20:08:04 -0500

    Few more scripts

---
+++ game_start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+screen -S uhc -p 0 -X stuff "spreadplayers 0 0 500 3000 true @a$(printf \\r)"
--- world_setup.sh
+++ world_setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 # Sets the world border center at 0,0
 screen -S uhc -p 0 -X stuff "worldborder center 0 0$(printf \\r)"
-
+sleep 1
 # The world border square will be x wide. So -1/2x to +1/2x.
 screen -S uhc -p 0 -X stuff "worldborder set 3000 0$(printf \\r)"
-
+sleep 1
 # Set me to spectator mode
 screen -S uhc -p 0 -X stuff "gamemode 3 repkam09$(printf \\r)"
-
+sleep 1
 # Move me to the center of the map
 screen -S uhc -p 0 -X stuff "tp repkam09 0 100 0$(printf \\r)"
+sleep 1
+
+# Set some game rules
+screen -S uhc -p 0 -X stuff "gamerule naturalRegeneration false$(printf \\r)"
+sleep 1
+screen -S uhc -p 0 -X stuff "gamerule reducedDebugInfo true$(printf \\r)"
+sleep 1
