30c79c3946a2cfc4b0503794dde1eb4f364463d4
Mark Repka <mark@repkam09.com>
2016-02-24 21:49:00 -0500

    Install vim and use mc 1.9

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,4 @@
 sudo apt-get update
 sudo apt-get upgrade
 wget https://s3.amazonaws.com/Minecraft.Download/versions/1.9-pre3/minecraft_server.1.9-pre3.jar
-sudo apt-get install openjdk-8-jre-headless screen git 
+sudo apt-get install openjdk-8-jre-headless screen git vim
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -S uhc java -Xmx4096M -Xms4096M -jar minecraft_server.1.8.9.jar nogui
+screen -S uhc java -Xmx4096M -Xms4096M -jar minecraft_server.1.9-pre3.jar nogui
